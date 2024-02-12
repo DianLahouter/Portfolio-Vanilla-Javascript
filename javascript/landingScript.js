9073ca8efdc53af0d5e4c4e9cc3ab8380cbdf916
DianLahouter <111975389+DianLahouter@users.noreply.github.com>
2024-02-12 17:04:54 +0200

    More Signs of life on landing page

--- javascript/landingScript.js
+++ javascript/landingScript.js
@@ -1,5 +1,7 @@
-const monitorImg = document.getElementById("monitorImg");
-const monitorScreen = document.getElementById("monitorScreen");
+var monitorImg = document.getElementById("monitorImg");
+var monitorScreen = document.getElementById("monitorScreen");
+var myName = document.getElementById("myName");
+var myTitle = document.getElementById("myTitle");
 
 changeScreenSize();
 
@@ -10,3 +12,38 @@ function changeScreenSize() {
 
 const observer = new ResizeObserver(changeScreenSize);
 observer.observe(monitorImg);
+
+
+flickerRandomLetter(myName);
+flickerRandomLetter(myTitle);
+
+function flickerRandomLetter(wordElement){
+    var cleanWordElement = wordElement.innerHTML;
+    let textContent = wordElement.textContent;
+    let randomLetterIndex;
+    do {
+        randomLetterIndex = Math.floor(Math.random() * textContent.length);
+    } while (randomLetterIndex === 4);
+    
+
+    textContent = textContent.substring(0, randomLetterIndex) + '<span class="letterGlitch">' + textContent[randomLetterIndex] + '</span>' + textContent.substring(randomLetterIndex+1);
+
+    wordElement.innerHTML = textContent;
+
+    setTimeout(function() {
+        flickerRandomLetter(wordElement); 
+    }, Math.random() * (3000 - 1500) + 1500);
+
+    setTimeout(function() {
+        turnLettersBackOn(wordElement,cleanWordElement); 
+    }, Math.random() * (300 - 50) + 50);
+}
+
+
+function turnLettersBackOn(wordElement, cleanInnerHtml){
+    wordElement.innerHTML = cleanInnerHtml;
+}
+
+function flickerLetter(index){
+
+}

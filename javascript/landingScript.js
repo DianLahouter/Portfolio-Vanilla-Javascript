5b5db5def9756da211fb81dfb4a452578df508d0
DianLahouter <111975389+DianLahouter@users.noreply.github.com>
2024-02-12 18:55:28 +0200

    more signs of life

--- javascript/landingScript.js
+++ javascript/landingScript.js
@@ -2,6 +2,7 @@ var monitorImg = document.getElementById("monitorImg");
 var monitorScreen = document.getElementById("monitorScreen");
 var myName = document.getElementById("myName");
 var myTitle = document.getElementById("myTitle");
+var binaryNumbers = document.getElementsByClassName("binaryNumber");
 
 changeScreenSize();
 
@@ -16,6 +17,7 @@ observer.observe(monitorImg);
 
 flickerRandomLetter(myName);
 flickerRandomLetter(myTitle);
+spawnBinaryNumbers();
 
 function flickerRandomLetter(wordElement){
     var cleanWordElement = wordElement.innerHTML;
@@ -44,6 +46,44 @@ function turnLettersBackOn(wordElement, cleanInnerHtml){
     wordElement.innerHTML = cleanInnerHtml;
 }
 
-function flickerLetter(index){
-
+function deleteBinaryNumber(binaryNumber) {
+    setTimeout(function() {
+        binaryNumber.remove();
+    }, 4000); 
 }
+
+function spawnBinaryNumbers() {
+    var img = document.createElement('img');
+
+    const randomNumber = Math.random();
+    if (randomNumber < 0.4)
+        img.src = './images/landing/0.png';
+    else
+        img.src = './images/landing/1.png';
+
+    img.style.top = '0px';
+
+    // Calculate the width of each segment
+    var segmentWidth = screen.width / 10;
+
+    // Choose a random segment
+    var randomSegment = Math.floor(Math.random() * 13);
+
+    // Calculate the maximum left position to ensure the image fits within the window
+    var maxLeft = window.innerWidth - segmentWidth;
+
+    // Set the left position based on the chosen segment, ensuring it does not exceed the maximum left position
+    img.style.left = Math.min(randomSegment * segmentWidth, screen.width) + 'px';
+
+    img.classList.add('binaryNumber');
+
+    document.body.insertBefore(img, document.body.firstChild);
+
+    deleteBinaryNumber(img);
+
+    var randomDelay = Math.random() * 100 + 50;
+    setTimeout(spawnBinaryNumbers, randomDelay);
+}
+
+
+

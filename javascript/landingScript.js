f7c3fdddde0d59ce3807bb9018dd16e30c6b117b
DianLahouter <111975389+DianLahouter@users.noreply.github.com>
2024-02-13 08:29:52 +0200

    Fixed Matrix Lanes

--- javascript/landingScript.js
+++ javascript/landingScript.js
@@ -3,6 +3,7 @@ var monitorScreen = document.getElementById("monitorScreen");
 var myName = document.getElementById("myName");
 var myTitle = document.getElementById("myTitle");
 var binaryNumbers = document.getElementsByClassName("binaryNumber");
+var matrixLanes = [];
 
 changeScreenSize();
 
@@ -53,10 +54,17 @@ function deleteBinaryNumber(binaryNumber) {
 }
 
 function spawnBinaryNumbers() {
+    var totalLanes = 12;
+    var spawnPoint = 0;
     var img = document.createElement('img');
 
+    //Initiate lanes array
+    for(var i = 0; i < totalLanes ; i++){
+        matrixLanes.push(0);
+    }
+
     const randomNumber = Math.random();
-    if (randomNumber < 0.4)
+    if (randomNumber < 0.35)
         img.src = './images/landing/0.png';
     else
         img.src = './images/landing/1.png';
@@ -64,25 +72,43 @@ function spawnBinaryNumbers() {
     img.style.top = '0px';
 
     // Calculate the width of each segment
-    var segmentWidth = screen.width / 10;
+    var segmentWidth = screen.width / totalLanes;
+    spawnPoint += segmentWidth;
 
-    // Choose a random segment
-    var randomSegment = Math.floor(Math.random() * 13);
+    var randomSegment = Math.floor(Math.random() * totalLanes);
 
-    // Calculate the maximum left position to ensure the image fits within the window
-    var maxLeft = window.innerWidth - segmentWidth;
+    //Get unoccupied lane
+    while(matrixLanes[randomSegment] == 1){
+        randomSegment = Math.floor(Math.random() * totalLanes);
+    }
 
-    // Set the left position based on the chosen segment, ensuring it does not exceed the maximum left position
-    img.style.left = Math.min(randomSegment * segmentWidth, screen.width) + 'px';
+    var segmentCounter = 0;
+    while(segmentCounter < randomSegment){
+        spawnPoint += segmentWidth;
+        segmentCounter++;
+    }
 
+    img.style.left = spawnPoint + 'px';
     img.classList.add('binaryNumber');
-
     document.body.insertBefore(img, document.body.firstChild);
 
     deleteBinaryNumber(img);
+    occupyLane(randomSegment);
 
-    var randomDelay = Math.random() * 100 + 50;
+    var randomDelay = Math.random() * 60 + 30;
     setTimeout(spawnBinaryNumbers, randomDelay);
+}
+
+function occupyLane(index){
+    matrixLanes[index] = 1;
+
+    setTimeout(function() {
+        unOccupyLane(index); 
+    }, 140);
+}
+
+function unOccupyLane(index){
+    matrixLanes[index] = 0;
 }
 
 
